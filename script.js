0c297874f764a445ce2d273a135a53a393a6d044
unknown <vijayragul8898@gmail.com>
2024-02-07 11:39:11 +0530

    ragul

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+let navBar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navBar.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
 
 
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
-    navbar.classList.remove('active');
+    navBar.classList.remove('active');
 }
 
-let navbar = document.querySelector('.header .navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
-}
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
